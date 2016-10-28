7a2ea9ed0c9fa9f9ee5e5a71e4f069ea97274012
Maik Franke <maik.franke@datenwerk.de>
2016-10-28 16:57:45 +0200

    initialize teampassword backup project

---
+++ bin/console.php
@@ -0,0 +1,35 @@
+#!/usr/bin/env php
+<?php
+require __DIR__.'/../vendor/autoload.php';
+
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class TeamPasswordBackupCommand
+ */
+class TeamPasswordBackupCommand extends Command
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this->setName('teampassword:backup');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln("Hello world!");
+    }
+}
+
+$application = new Application();
+$application->add(new TeamPasswordBackupCommand());
+$application->run();
